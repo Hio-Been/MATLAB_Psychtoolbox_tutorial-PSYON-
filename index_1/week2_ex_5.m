b60c17ea50c5fd091e408351093a2ed8fc4001a7
Hio-been Han <hiobeen@kist.re.kr>
2016-01-31 13:13:31 +0900

    possible error correction
    using ‘size’ as a variable name may cause a confusion (there is a
    built-in method name of ‘size’)

--- index_1/week2_ex_5.m
+++ index_1/week2_ex_5.m
@@ -4,9 +4,9 @@
 clear all;
 
 matrix = zeros(10,10);
-size = size(matrix);
-x_length = size(1);
-y_length = size(2);
+sizes = size(matrix);
+x_length = sizes(1);
+y_length = sizes(2);
 
 imagesc(matrix);
 colormap(gray);
@@ -18,11 +18,13 @@ while index < (x_length * y_length)+1
         matrix(index-1) = index/(x_length * y_length);
     end
     imagesc(matrix)
-    pause(0.00005)
+    pause(0.005)
     index = index + 1;
     colormap(winter);
-
+    
+    disp(index) % to display index in a command window
+    
 end
-
+disp('end')
 clear all
 %clc
